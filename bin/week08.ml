e62a62827ca815cb8f5545bc28618dea005f8378
Ryosuke SATO <ryosuke@kb.is.s.u-tokyo.ac.jp>
2022-06-07 14:25:31 +0900

    Fix Week08

--- bin/week08.ml
+++ bin/week08.ml
@@ -6,9 +6,11 @@ let ty_subst = Type("ty_subst", "subst -> ty -> ty")
 let compose = Type("compose", "subst -> subst -> subst")
 let unify = Type("unify", "(ty * ty) list -> subst")
 
-let toi n = Toi(Dir, n)
+let toi n = Toi(ML, n)
 
 let assignments =
-  [toi 2, [ty; subst; ty_subst];
-   toi 3, [ty; subst; compose];
-   toi 4, [ty; subst; unify]]
+  [Toi(ML, 2), [ty; subst; ty_subst];
+   Toi(ML, 3), [ty; subst; compose];
+   Toi(ML, 4), [ty; subst; unify];
+   Toi(Dir, 5), [];
+   Hatten(Dir, 2), []]
